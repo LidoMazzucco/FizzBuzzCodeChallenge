ca8c4c22003fb710c6f5e31e0d70c3be3c50e53a
Lido Mazzucco <lido.mazzucco@signavio.com>
2021-11-28 12:14:50 +0100

    add Buzz test

--- src/test/java/com/signavio/solution/FizzBuzzTest.java
+++ src/test/java/com/signavio/solution/FizzBuzzTest.java
@@ -6,11 +6,38 @@ import org.junit.jupiter.api.Test;
 public class FizzBuzzTest {
 
     @Test
-    public void checkFizz_numberMultipleofThree_returnFizz()
+    public void checkFizz_numberMultipleOfThree_returnFizz()
     {
         int testNumber = 3;
         String result = FizzBuzz.checkFizz(testNumber);
 
         Assertions.assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void checkFizz_numberMultipleNotOfThree_returnEmpty()
+    {
+        int testNumber = 10;
+        String result = FizzBuzz.checkFizz(testNumber);
+
+        Assertions.assertEquals("", result);
+    }
+
+    @Test
+    public void checkFizz_numberMultipleOfFive_returnBuzz()
+    {
+        int testNumber = 10;
+        String result = FizzBuzz.checkBuzz(testNumber);
+
+        Assertions.assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void checkFizz_numberMultipleNotOfFive_returnEmpty()
+    {
+        int testNumber = 13;
+        String result = FizzBuzz.checkFizz(testNumber);
+
+        Assertions.assertEquals("", result);
     }
 }
