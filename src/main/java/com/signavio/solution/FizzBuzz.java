9d1a3b55f6b3dd75c8c86bed111ee52375cd9842
Lido Mazzucco <lido.mazzucco@signavio.com>
2021-11-29 08:30:59 +0100

    correct solving problem

--- src/main/java/com/signavio/solution/FizzBuzz.java
+++ src/main/java/com/signavio/solution/FizzBuzz.java
@@ -2,6 +2,8 @@ package com.signavio.solution;
 
 public class FizzBuzz {
 
+    private static final int START_POINT = 1;
+    private static final int END_POINT = 100;
     public static final String FIZZ_MESSAGE  = "Fizz";
     public static final String BUZZ_MESSAGE = "Buzz";
     public static final String WHIZ_MESSAGE = "Whiz";
@@ -10,22 +12,27 @@ public class FizzBuzz {
 
 
     public static void main(String[] args) {
-        String value = "";
-        Integer number = 1;
-        while (number <= 100) {
-            value = fizzBuzzProblem(number);
-            System.out.println(number + ": " + value);
-            number++;
+        String output;
+        int number = START_POINT;
 
+        while (number <= END_POINT) {
+            output = fizzBuzzProblem(number);
+            System.out.println(number + ": " + output);
+            number++;
         }
     }
 
     public static String fizzBuzzProblem(int number) {
         StringBuilder value = new StringBuilder();
 
-        value.append(checkFizzBuzz(number, FIZZ_MESSAGE, FIZZ_CONDITION));
-        value.append(checkFizzBuzz(number, BUZZ_MESSAGE, BUZZ_CONDITION));
         value.append(checkWhiz(number));
+
+        if(value.length() == 0){
+            value.append(checkFizzBuzz(number, FIZZ_MESSAGE, FIZZ_CONDITION));
+            value.append(checkFizzBuzz(number, BUZZ_MESSAGE, BUZZ_CONDITION));
+        }
+
+        if(value.length() == 0) value.append(number);
 
         return value.toString();
     }
